0154a2a24b09230a49c53bf40eee4c19e142ddbd
Fabio Fernandez <kingofnoobs845@gmail.com>
2025-12-27 12:03:54 -0400

    haciendo merge

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,9 +1,9 @@
 import { AuthController } from '../controllers/AuthController.js'
 import { Router } from 'express'
-
 const router = Router()
 
 
 router.post('/create-account', AuthController.createAccount)
-
+router.get('/list-users', AuthController.listUsers)
+router.post('/login', AuthController.login)
 export default router
